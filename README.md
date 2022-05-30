00b542e12aab61a236a31d9a61c09bb0adcc3d39
Nataly Araujo <ntlyaraujo@gmail.com>
2022-05-30 10:06:45 +0200

    Clean old code and update README

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ In the project directory, you can run:
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-## What inbuilt functions are we going to focus on?
+## What functionalities are we going to focus on?
 
 - Responsiveness (works on mobile, tablet, laptop and desktop).
 - Tested browsers: Opera GX LVL3(core: 86.0.4363.64), Google Chrome (102.0.5005.63), Firefox (100.0.2) and Edge (101.0.1210.53).
--- src/core/contexts/index.tsx
+++ src/core/contexts/index.tsx
@@ -11,24 +11,3 @@ copy: emptyShow, // set a default value
 setCopy: () => {},
 })
 export const useGlobalContext = () => useContext(MyGlobalContext)
-/* interface IShowContext {
-  showDetails: ShowDetails ;
-  setShowDetails: React.Dispatch<React.SetStateAction<Show>>
-}
-
-type Props = {
-  children?: React.ReactNode;
-};
-
-export const ShowContext = createContext<IShowContext | null>(null);
-
-export const ShowProvider = (props: Props) => {
-   
-  const [showDetails, setShowDetails] = useState();
-  const value:IShowContext = {showDetails, setShowDetails };
-
-  return (
-    <ShowContext.Provider value={value}>{props.children}</ShowContext.Provider>
-  );
-};
- */
